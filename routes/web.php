316b34c0ce4bd0423ca57b695fcad40aac2e668b
zhongjt2016 <594464333@qq.com>
2019-06-09 07:41:19 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::get('/help', 'StaticPagesController@help')
     ->name('help');
 Route::get('/about', 'StaticPagesController@about')
     ->name('about');
+
+Route::get('signup', 'UsersController@create')
+    ->name('signup');
