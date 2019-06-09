3b3fa84771868ba0341a4430924034c2aefcf15a
zhongjt2016 <594464333@qq.com>
2019-06-09 15:58:36 +0000

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,11 +4,13 @@
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
+
 <body>
 @include('layouts._header')
 
 <div class="container">
     <div class="offset-md-1 col-md-10">
+        @include('shared._messages')
         @yield('content')
         @include('layouts._footer')
     </div>
