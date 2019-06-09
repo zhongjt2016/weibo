b703761d5888c26c6c5bcd943ef225e957461a6c
zhongjt2016 <594464333@qq.com>
2019-06-09 16:20:09 +0000

    fix

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,17 @@
     return view('welcome');
 });*/
 
-
+//静态页
 Route::get('/', 'StaticPagesController@home')
     ->name('home');
 Route::get('/help', 'StaticPagesController@help')
     ->name('help');
 Route::get('/about', 'StaticPagesController@about')
     ->name('about');
-
+//注册
 Route::get('signup', 'UsersController@create')
     ->name('signup');
+//登录登出
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
