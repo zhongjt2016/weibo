9e2beda5a01fc77b63a8afffadb863d75dcdddb5
zhongjt2016 <594464333@qq.com>
2019-06-11 16:47:52 +0000

    重置密码

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,8 @@ Route::resource('users', 'UsersController');
 
 Route::get('/signup/user/{id}/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
