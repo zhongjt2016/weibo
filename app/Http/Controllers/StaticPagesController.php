2c44d2b5b67032fd45ee0593b0446e2d6725993f
zhongjt2016 <594464333@qq.com>
2019-06-12 15:49:22 +0000

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -11,7 +11,7 @@ class StaticPagesController extends Controller
     {
         $feed_items = [];
         if (Auth::check()) {
-            $feed_items = Auth::user()->feed()->paninate(30);
+            $feed_items = Auth::user()->feed()->paginate(30);
         }
         return view('static_pages/home', compact('feed_items'));
     }
